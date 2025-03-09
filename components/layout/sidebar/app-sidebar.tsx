22755bdbe544e78c28956ccffb9385a9470a75ff
Thimoté Fétu <thimote.fetu@student.vinci.be>
2025-03-10 00:32:07 +0100

    fix pnpm-lock.yaml

--- components/layout/sidebar/app-sidebar.tsx
+++ components/layout/sidebar/app-sidebar.tsx
@@ -13,7 +13,6 @@ import { Button } from '@/components/ui/button';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from '@/components/ui/sidebar';
 import { Team, teams } from '@/mock-data/teams';
 import Link from 'next/link';
-import { X } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 const data = {
@@ -49,7 +48,6 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-   const [open, setOpen] = useState(true);
    const [teamList, setTeamList] = useState<Team[]>([]);
 
    useEffect(() => {
@@ -72,37 +70,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
          </SidebarContent>
          <SidebarFooter>
             <div className="w-full flex flex-col gap-2">
-               {open && (
-                  <div className="group/sidebar relative flex flex-col gap-2 rounded-lg border p-4 text-sm w-full">
-                     <div
-                        className="absolute top-2.5 right-2 z-10 cursor-pointer"
-                        onClick={() => setOpen(!open)}
-                        role="button"
-                     >
-                        <X className="size-4" />
-                     </div>
-                     <div className="text-balance text-lg font-semibold leading-tight group-hover/sidebar:underline">
-                        Fine components coded by lndev.
-                     </div>
-                     <div>
-                        A fun collection of small, well-coded components to streamline your
-                        development process.
-                     </div>
-                     <Link
-                        target="_blank"
-                        rel="noreferrer"
-                        className="absolute inset-0"
-                        href="https://ui.lndev.me"
-                     >
-                        <span className="sr-only">Deploy to Vercel</span>
-                     </Link>
-                     <Button size="sm" className="w-full">
-                        <Link href="https://ui.lndev.me" target="_blank" rel="noopener noreferrer">
-                           ui.lndev.me
-                        </Link>
-                     </Button>
-                  </div>
-               )}
                <div className="w-full flex items-center justify-between">
                   <HelpButton />
                   <Button size="icon" variant="secondary" asChild>
